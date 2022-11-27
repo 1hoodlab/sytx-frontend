f49dbd232878e936c763b29b3734c7e07878028f
hoandoice <hoan.do@icetea.io>
2022-11-27 14:28:14 +0700

    refactor: update args ls

--- components/terminal/commands/ls.ts
+++ components/terminal/commands/ls.ts
@@ -5,7 +5,7 @@ import { colorize, getSpacing } from "../utils";
 
 const ls: SystemCommand = {
   id: "ls",
-  args: 2,
+  args: 1,
   description: "Get list events or method of smart contract",
   usage: "ls event | ls method",
   exec: async (term: Terminal, args: string[]) => {},
